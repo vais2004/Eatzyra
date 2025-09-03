5e061b7b66daf0bb4aec10658752d1958de6658c
vais2004 <vkawale2004@gmail.com>
2025-09-03 10:10:56 +0530

    update

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Carousal from "../components/Carousal";
 
 export default function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+   const loadData = async () => {
+    try {
+      let response = await fetch("http://localhost:5000/api/food-data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      let json = await response.json();
+      console.log(json);
+
+      // assign response data
+      setFoodItem(json.foodItems || []);
+      setFoodCat(json.categories || []);
+    
+    } catch (err) {
+      console.error("Error loading data:", err);
+     
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <div>
       <div>
         <Header />
       </div>
       <div>
-        <Carousal/>
+        <Carousal />
       </div>
-      <div className="m-4">
+      <div className="container m-4">
         <Card />
       </div>
       <div>
